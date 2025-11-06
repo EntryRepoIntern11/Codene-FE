0f74d232031f453331c37a0d0991c90ccd674214
강세아 <gangsea817@gmail.com>
2025-11-06 22:47:33 +0900

    feat: header & 로그인, 회원가입 퍼블리싱

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -18,8 +18,7 @@ const Login = () => {
           </Link>
           <TextBox>
             <LoginText href="/login">Login</LoginText>
-              <MyPageText href="/MyPage">MyPage</MyPageText>
-            
+            <MyPageText href="/MyPage">MyPage</MyPageText>
           </TextBox>
         </Header>
         <hr />
@@ -31,28 +30,35 @@ const Login = () => {
           </SelectKate>
         </Kategorie>
         <hr />
-        <LoginTotalBox>
-          <LoginBox>
-            <LoginTitle>로그인</LoginTitle>
+        <SignUpTotalBox>
+          <SignUpBox>
+            <SignUpTitle>회원가입</SignUpTitle>
             <hr />
             <NumberContainer>
-              <LoginSpan>학번</LoginSpan>
-              <LoginInput type="text" placeholder="학번을 입력해주세요" />
+              <SignUpSpan>학번</SignUpSpan>
+              <SignUpInput type="text" placeholder="학번을 입력해주세요" />
             </NumberContainer>
             <PwdContainer>
-              <LoginSpan>비밀번호</LoginSpan>
-              <LoginInput
+              <SignUpSpan>비밀번호</SignUpSpan>
+              <SignUpInput
                 type="password"
                 placeholder="비밀번호를 입력해주세요"
               />
             </PwdContainer>
-            <LoginButton>로그인</LoginButton>
-            <NoUser>
-              계정이 없으신가요?
-              <JoinUser href="/signup">회원가입</JoinUser>
-            </NoUser>
-          </LoginBox>
-        </LoginTotalBox>
+            <CheckPwdContainer>
+              <SignUpSpan>비밀번호 확인</SignUpSpan>
+              <SignUpInput
+                type="password"
+                placeholder="비밀번호를 다시 입력해주세요"
+              />
+            </CheckPwdContainer>
+            <SignUpButton>로그인</SignUpButton>
+            <YesUser>
+              계정이 있으신가요?
+              <LoginUser href="/login">로그인</LoginUser>
+            </YesUser>
+          </SignUpBox>
+        </SignUpTotalBox>
       </SecondBox>
     </Body>
   );
@@ -130,11 +136,11 @@ const CodeSpan = styled.span`
   font-size: 25px;
 `;
 
-const LoginTotalBox = styled.div`
+const SignUpTotalBox = styled.div`
   display: flex;
   justify-content: center;
 `;
-const LoginBox = styled.div`
+const SignUpBox = styled.div`
   width: 535px;
   min-height: 485px;
   display: flex;
@@ -145,26 +151,11 @@ const LoginBox = styled.div`
     margin-bottom: 54px;
   }
 `;
-const LoginTitle = styled.span`
+
+const SignUpTitle = styled.span`
   font-size: 30px;
   margin-bottom: 47px;
-`;
-
-const LoginSpan = styled.span`
-  font-size: 14px;
-  color: #555555;
-  margin-left: 7px;
-`;
-
-const LoginInput = styled.input`
-  width: 100%;
-  height: 40px;
-  border-radius: 4px;
-  padding-left: 20px;
-  border: 1px solid #999999;
-  margin-top: 7px;
-  margin-bottom: 46px;
-`;
+`
 
 const NumberContainer = styled.div`
   display: flex;
@@ -175,7 +166,40 @@ const PwdContainer = styled.div`
   flex-direction: column;
 `;
 
-const LoginButton = styled.button`
+const SignUpSpan = styled.span`
+  font-size: 14px;
+  color: #555555;
+  margin-left: 7px;
+`;
+
+const SignUpInput = styled.input`
+  width: 100%;
+  height: 40px;
+  border-radius: 4px;
+  padding-left: 20px;
+  border: 1px solid #999999;
+  margin-top: 7px;
+  margin-bottom: 46px;
+`;
+
+const CheckPwdContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const YesUser = styled.span`
+  display: flex;
+  justify-content: center;
+  padding-top: 24px;
+  padding-bottom: 189px;
+`;
+
+const LoginUser = styled.a`
+  color: #3f51b5;
+  text-decoration: none;
+`;
+
+const SignUpButton = styled.button`
   color: white;
   border: none;
   background-color: #9fa8da;
@@ -189,16 +213,5 @@ const LoginButton = styled.button`
     background-color: #3f51b5;
     opacity: 1;
   }
-`;
-
-const NoUser = styled.span`
-  display: flex;
-  justify-content: center;
-  padding-top: 24px;
-  padding-bottom: 189px;
-`;
-const JoinUser = styled.a`
-  color: #3f51b5;
-  text-decoration: none;
 `;
 export default Login;
